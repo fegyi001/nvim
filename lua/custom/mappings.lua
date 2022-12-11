a23b2185b919da01eafbd8d5d4416614fa7cfa27
Gergely Padányi-Gulyás <gergely.padanyi-gulyas@dkfkft.hu>
2022-12-11 08:37:48 +0100

    remap n mode column to '

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -2,12 +2,16 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "command mode", opts = { nowait = true } },
+    ["'"] = { ":", "command mode", opts = { nowait = true } },
     ["<leader>gg"] = { ":LazyGit<CR>" },
     ["<C-d>"] = { "<C-d>zz" },
     ["<C-u>"] = { "<C-u>zz" },
     ["n"] = { "nzzzv" },
     ["N"] = { "Nzzzv" },
+    ["[e"] = { ":<C-u>execute 'move -1-'. v:count1<cr>" },
+    ["]e"] = { ":<C-u>execute 'move +'. v:count1<cr>" },
+    ["[<space>"] = { ":<c-u>put! =repeat(nr2char(10), v:count1)<cr>" },
+    ["]<space>"] = { ":<c-u>put =repeat(nr2char(10), v:count1)<cr>" },
   },
 }
 
