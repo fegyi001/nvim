eab2e22fc9fe73115e41a9c9ad025ee164db31a8
Padányi-Gulyás Gergely <gergely.padanyi-gulyas@idomsoft.hu>
2023-02-19 13:52:22 +0100

    customize lazyvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,33 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume" }
+)
+
+vim.keymap.set("n", "'", ":", { nowait = true, desc = "Command mode" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
+vim.keymap.set("i", "<C-e>", "<End>")
+
+-- inoremap <C-b> <ESC>^i
+
+-- M.general = {
+--   n = {
+--     ["[e"] = { ":<C-u>execute 'move -1-'. v:count1<cr>" },
+--     ["]e"] = { ":<C-u>execute 'move +'. v:count1<cr>" },
+--     ["[<space>"] = { ":<c-u>put! =repeat(nr2char(10), v:count1)<cr>" },
+--     ["]<space>"] = { ":<c-u>put =repeat(nr2char(10), v:count1)<cr>" },
+--   },
+-- }
