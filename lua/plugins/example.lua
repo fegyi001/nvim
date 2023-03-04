a21f9cdd754437f011dd7f7740459e9a60bdbdd9
Padányi-Gulyás Gergely <gergely.padanyi-gulyas@idomsoft.hu>
2023-03-04 13:58:49 +0100

    fix prettier formatting

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -19,6 +19,22 @@ return {
       colorscheme = "tokyonight",
     },
   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "mason.nvim" },
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.formatting.prettierd,
+          nls.builtins.formatting.stylua,
+          nls.builtins.formatting.stylelint,
+          nls.builtins.diagnostics.flake8,
+        },
+      }
+    end,
+  },
   --
   -- -- change trouble config
   -- {
