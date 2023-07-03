e93100637f2f0fd00a73ed3293b2162d17b670c0
Padányi-Gulyás Gergely <gergely.padanyi-gulyas@idomsoft.hu>
2023-07-03 22:11:11 +0200

    keymaps for zenmode, disable twilight

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,6 +17,7 @@ vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Window left
 vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Window right" })
 vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Window down" })
 vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Window up" })
+vim.keymap.set("n", "<leader>m", "<cmd> ZenMode<CR>", { desc = "Toggle ZenMode" })
 
 vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("i", "<C-h>", "<Left>")
