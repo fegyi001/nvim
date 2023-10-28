a73afb7e1956af63706abbd531779d6de41510fc
Padányi-Gulyás Gergely <gergely.padanyi-gulyas@idomsoft.hu>
2023-10-28 20:35:35 +0200

    add nvim-ufo

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -20,6 +20,7 @@ return {
       "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
       { desc = "Open harpoon quick menu" }
     )
+    keymap.set("n", "<leader>ht", "<cmd>Telescope harpoon marks<cr>", { desc = "Find telescope harpoon marks" })
     keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
     keymap.set(
       "n",
@@ -27,29 +28,10 @@ return {
       "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
       { desc = "Go to previous harpoon mark" }
     )
-    keymap.set(
-      "n",
-      "<leader>1",
-      "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
-    keymap.set(
-      "n",
-      "<leader>2",
-      "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
-    keymap.set(
-      "n",
-      "<leader>3",
-      "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
-    keymap.set(
-      "n",
-      "<leader>4",
-      "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
+    keymap.set("n", "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", { desc = "Go to harpoon mark 1" })
+    keymap.set("n", "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", { desc = "Go to harpoon mark 2" })
+    keymap.set("n", "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", { desc = "Go to harpoon mark 3" })
+    keymap.set("n", "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", { desc = "Go to harpoon mark 4" })
+    keymap.set("n", "<leader>5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", { desc = "Go to harpoon mark 5" })
   end,
 }
---
+++ lua/plugins/nvim-ufo.lua
@@ -0,0 +1,29 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+  },
+  config = function()
+    local ufo = require("ufo")
+    vim.o.foldcolumn = "1" -- '0' is not bad
+    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+
+    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+    vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
+    vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
+    vim.keymap.set("n", "zK", function()
+      local winid = ufo.peekFoldedLinesUnderCursor()
+      if not winid then
+        vim.lsp.buf.hover()
+      end
+    end, { desc = "Peek fold" })
+
+    ufo.setup({
+      provider_selector = function(bufnr, filetype, buftype)
+        return { "lsp", "indent" }
+      end,
+    })
+  end,
+}
