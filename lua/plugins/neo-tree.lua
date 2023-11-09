dac00ef0350afccf8f9dbd6e3662543645b2f313
Padányi-Gulyás Gergely <gergely.padanyi-gulyas@idomsoft.hu>
2023-11-09 22:39:25 +0100

    add harpoon to neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -59,6 +59,30 @@ return {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
+      components = {
+        harpoon_index = function(config, node, state)
+          local Marked = require("harpoon.mark")
+          local path = node:get_id()
+          local success, index = pcall(Marked.get_index_of, path)
+          if success and index and index > 0 then
+            return {
+              text = string.format(" -> %d", index), -- <-- Add your favorite harpoon like arrow here
+              highlight = config.highlight or "NeoTreeDirectoryIcon",
+            }
+          else
+            return {}
+          end
+        end,
+      },
+      renderers = {
+        file = {
+          { "icon" },
+          { "name", use_git_status_colors = true },
+          { "harpoon_index" }, --> This is what actually adds the component in where you want it
+          { "diagnostics" },
+          { "git_status", highlight = "NeoTreeDimText" },
+        },
+      },
     },
     window = {
       mappings = {
