c98f28101ecfd7bc969a6dba21a1df5e7763f09a
Gergely Padányi-Gulyás <gergely.padanyi-gulyas@idomsoft.hu>
2024-12-12 11:00:04 +0100

    add mini.move, package-info, fidget

---
+++ lua/fegyi001/plugins/mini-move.lua
@@ -0,0 +1,17 @@
+return {
+	"echasnovski/mini.move",
+	event = "VeryLazy",
+	opts = {
+		mappings = {
+			left = "<a-s-h>",
+			right = "<a-s-l>",
+			down = "<a-s-j>",
+			up = "<a-s-k>",
+
+			line_left = "",
+			line_right = "",
+			line_down = "",
+			line_up = "",
+		},
+	},
+}
