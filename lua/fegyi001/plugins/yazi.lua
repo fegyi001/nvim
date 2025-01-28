cef93486921abfa2e3308233a6d21c2f02f83f86
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-01-28 19:51:37 +0100

    yazi

--- lua/fegyi001/plugins/yazi.lua
+++ lua/fegyi001/plugins/yazi.lua
@@ -3,24 +3,24 @@ return {
 	event = "VeryLazy",
 	keys = {
 		{
-			"<leader>cc",
+			"<leader>yy",
 			"<cmd>Yazi<cr>",
 			desc = "Open yazi at the current file",
 		},
 		{
-			"<leader>cw",
+			"<leader>yc",
 			"<cmd>Yazi cwd<cr>",
 			desc = "Open the file manager in nvim's working directory",
 		},
-		-- {
-		-- 	"<c-up>",
-		-- 	"<cmd>Yazi toggle<cr>",
-		-- 	desc = "Resume the last yazi session",
-		-- },
+		{
+			"<leader>yr",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
 	},
 	opts = {
 		-- if you want to open yazi instead of netrw, see below for more info
-		open_for_directories = false,
+		open_for_directories = true,
 		keymaps = {
 			show_help = "<f1>",
 		},
