0d3c19e4cebe2d0018540d14d2138b0f5450f7d4
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-02-09 13:54:32 +0100

    grug-far

--- lua/fegyi001/plugins/grug-far.lua
+++ lua/fegyi001/plugins/grug-far.lua
@@ -2,13 +2,11 @@ return {
 	"MagicDuck/grug-far.nvim",
 	event = "VeryLazy",
 	config = function()
-		local grug = require("grug-far")
 		local keymap = vim.keymap -- for conciseness
 		keymap.set("n", "<leader>r", "<cmd>:GrugFar<cr>", { desc = "Search and replace" })
 		require("grug-far").setup({
-			-- options, see Configuration section below
-			-- there are no required options atm
 			-- engine = 'ripgrep' is default, but 'astgrep' can be specified
+			-- engine = "astgrep",
 		})
 	end,
 }
