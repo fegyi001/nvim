7f116d9be19591aecb6742c039a044050f074c8b
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-02-14 12:58:43 +0100

    angular.lua

---
+++ lua/plugins/lsp/angular.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        angularls = {
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern("nx.json", "angular.json")(fname)
+                or vim.loop.cwd()
+          end,
+        },
+      },
+    },
+  },
+}
