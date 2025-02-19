9de7064b2f83cb28651d1bf20d26ef4302a03687
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-02-19 10:54:14 +0100

    asd

--- lua/plugins/nvim-spider.lua
+++ lua/plugins/nvim-spider.lua
@@ -1,6 +1,15 @@
 return {
   "chrisgrieser/nvim-spider",
   opts = {},
+  enabled = false,
+  config = function()
+    require("nvim-spider").setup({
+      -- skipInsignificantPunctuation = false,
+      -- consistentOperatorPending = false, -- see "Consistent Operator-pending Mode" in the README
+      -- subwordMovement = true,
+      -- customPatterns = {}, -- check "Custom Movement Patterns" in the README for details
+    })
+  end,
   keys = {
     {
       "w",
