23a5afdbefbef6c08bebf3e59f89b9ca39f0c992
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-02-27 09:59:19 +0100

    nx

--- lua/plugins/nx.lua
+++ lua/plugins/nx.lua
@@ -3,13 +3,11 @@ return {
     "Equilibris/nx.nvim",
     dependencies = {
       "nvim-telescope/telescope.nvim",
-      -- "ibhagwan/fzf-lua",
     },
+    event = "VeryLazy",
     opts = {
-      -- See below for config options
       nx_cmd_root = "npx nx",
     },
-    -- Plugin will load when you use these keys
     keys = {
       { "<leader>nxr", "<cmd>Telescope nx actions<CR>", desc = "nx actions" },
       { "<leader>nxg", "<cmd>Telescope nx generators<CR>", desc = "nx generators" },
