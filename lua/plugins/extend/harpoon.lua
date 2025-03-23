e402668b8480ff71fe2c5ce95ec1c8b88aac04e3
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-03-23 17:54:28 +0100

    harpoon, trouble

--- lua/plugins/extend/harpoon.lua
+++ lua/plugins/extend/harpoon.lua
@@ -1,7 +1,6 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-  depencencies = { "nvim-telescope/telescope.nvim" },
   opts = {
     settings = {
       save_on_toggle = true,
@@ -39,31 +38,5 @@ return {
       })
     end
     return keys
-  end,
-  config = function()
-    local harpoon = require("harpoon")
-    local telescope = require("telescope")
-    local telescope_config = require("telescope.config")
-    harpoon:setup({})
-    local conf = telescope_config.values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-      require("telescope.pickers")
-        .new({}, {
-          prompt_title = "Harpoon",
-          finder = require("telescope.finders").new_table({
-            results = file_paths,
-          }),
-          previewer = conf.file_previewer({}),
-          sorter = conf.generic_sorter({}),
-        })
-        :find()
-    end
-    vim.keymap.set("n", "<leader>ht", function()
-      toggle_telescope(harpoon:list())
-    end, { desc = "Open Harpoon in Telescope" })
   end,
 }
