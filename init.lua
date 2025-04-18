64080015ca96e3d3aa411b957c01b540f6865628
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-04-18 13:13:42 +0200

    registers

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.icons")
+
+-- registers
+-- https://www.youtube.com/watch?v=Y3XWijJgdJs
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
