ccd91ea44cdb24c6e15940b05e4bf050114b935f
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-05-27 13:35:14 +0200

    updates

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,7 +1,7 @@
 return {
   "m4xshen/hardtime.nvim",
-  -- enabled = false,
-  lazy = false,
+  enabled = false,
+  event = "VeryLazy",
   dependencies = { "MunifTanjim/nui.nvim" },
   opts = {},
 }
