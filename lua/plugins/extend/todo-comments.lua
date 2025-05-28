e3f8881c7ed23548441faf6699d31245aa1fe418
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-05-28 13:39:12 +0200

    asdf

--- lua/plugins/extend/todo-comments.lua
+++ lua/plugins/extend/todo-comments.lua
@@ -11,9 +11,9 @@ return {
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-      { "<leader>xt", "<cmd>Trouble todo toggle filter={buf=0,tag={TODO,FIX,FIXME}}<cr>", desc = "Todo (Trouble) current file" },
+      { "<leader>xt", "<cmd>Trouble todo toggle filter={buf=0,tag={TODO,FIX,FIXME,PERF,HACK}}<cr>", desc = "Todo (Trouble) current file" },
       { "<leader>xT", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
-      { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+      { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME,PERF,HACK,WARNING<cr>", desc = "Todo (Telescope)" },
       { "<leader>sT", "<cmd>TodoTelescope<cr>", desc = "Todo" },
     },
 }
