69edcf9ca49c4ce201b90ae7442b3fe4c73834de
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-06-13 08:18:27 +0200

    flash tweak

---
+++ lua/plugins/extend/flash.lua
@@ -0,0 +1,31 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    label = {
+      rainbow = {
+        enabled = true,
+        shade = 4,
+      },
+    },
+    modes = {
+      char = {
+        jump_labels = true,
+        highlight = {
+          backdrop = false,
+        },
+      },
+    },
+    highlight = {
+      backdrop = false,
+    },
+  },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      -- { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+}
